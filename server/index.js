c5b61df903470f90480972792ecfbbf80c940a24
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-08 12:53:00 +0530

    project-setup and  install packages

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan());
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
