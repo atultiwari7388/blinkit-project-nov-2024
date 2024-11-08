4cfdb03f85f5a2a1ec0954f538e3b8ed1a7b7972
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-08 18:35:00 +0530

    21. verify-forgot-password done

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -348,3 +348,68 @@ export async function forgotPassword(request, response) {
     });
   }
 }
+
+//verify forgot password otp
+export async function verifyForgotPasswordOtp(request, response) {
+  try {
+    const { email, otp } = request.body;
+
+    if (!email || !otp) {
+      return response.status(400).json({
+        message: "Provide required field email, otp.",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return response.status(400).json({
+        message: "Email not available",
+        error: true,
+        success: false,
+      });
+    }
+
+    const currentTime = new Date().toISOString();
+
+    if (user.forgot_password_expiry < currentTime) {
+      return response.status(400).json({
+        message: "Otp is expired",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (otp !== user.forgot_password_otp) {
+      return response.status(400).json({
+        message: "Invalid otp",
+        error: true,
+        success: false,
+      });
+    }
+
+    //if otp is not expired
+    //otp === user.forgot_password_otp
+
+    const updateUser = await UserModel.findByIdAndUpdate(user?._id, {
+      forgot_password_otp: "",
+      forgot_password_expiry: "",
+    });
+
+    return response.json({
+      message: "Verify otp successfully",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+//reset the password
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -7,6 +7,7 @@ import {
   uploadAvatarController,
   updateUserDetails,
   forgotPassword,
+  verifyForgotPasswordOtp,
 } from "../controllers/user.controllers.js";
 import auth from "../middleware/auth.js";
 import upload from "./../middleware/multer.js";
@@ -26,5 +27,6 @@ userRouter.put(
 
 userRouter.put("/update-user", auth, updateUserDetails);
 userRouter.put("/forgot-password", forgotPassword);
+userRouter.put("/verify-forgot-password-otp", verifyForgotPasswordOtp);
 
 export default userRouter;
