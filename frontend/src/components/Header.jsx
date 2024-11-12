11ff3d83159d3969544c2c455929116d19d5926a
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-12 23:27:28 +0530

    44. user state changes done in header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaAngleDown, FaAngleUp } from "react-icons/fa";
 import Search from "./Search";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
@@ -51,12 +51,23 @@ export default function Header() {
 
             {/** Login and Cart buttons for desktop */}
             <div className="hidden lg:flex items-center gap-6">
-              <button
-                onClick={redirectToLoginPage}
-                className="text-lg px-4 py-2 transition-colors hover:text-gray-300"
-              >
-                Login
-              </button>
+              {user?._id ? (
+                <div>
+                  <div className="flex items-center gap-2">
+                    <p>Account</p>
+                    <FaAngleDown />
+                    {/* <FaAngleUp /> */}
+                  </div>
+                </div>
+              ) : (
+                <button
+                  onClick={redirectToLoginPage}
+                  className="text-lg px-4 py-2 transition-colors hover:text-gray-300"
+                >
+                  Login
+                </button>
+              )}
+
               <button className="flex items-center gap-2 bg-primary-200 hover:bg-gray-950 px-4 py-3 rounded-lg text-white shadow-lg transition-transform transform hover:scale-105">
                 <GrCart size={24} className="animate-bounce" />
                 <p className="font-semibold">My Cart</p>
