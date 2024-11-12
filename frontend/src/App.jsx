de4da502c8caf25279794a934ef07eb3e54a946c
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-12 14:16:37 +0530

    25 . react-router-dom setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
+
 export default function App() {
   return (
     <main>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <Outlet />
     </main>
   );
 }
