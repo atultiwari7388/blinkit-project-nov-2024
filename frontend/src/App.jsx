da0036f30d646982f966f1f571f45512ec69d494
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-12 23:19:20 +0530

    43. fetching all user details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,16 @@ import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
 import { useEffect } from "react";
 import fetchUserDetails from "./utils/fetchUserDetails";
+import { setUserDetails } from "./reduxStores/userDetailsSlice";
+import { useDispatch } from "react-redux";
 
 export default function App() {
+  const dispatch = useDispatch();
+
   const fetchUser = async () => {
     const userData = await fetchUserDetails();
-    console.log("UserDate", userData.data);
+    // console.log("UserDate", userData.data);
+    dispatch(setUserDetails(userData.data));
   };
 
   useEffect(() => {
