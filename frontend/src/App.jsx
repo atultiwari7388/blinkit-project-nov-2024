3ceae78b228be185171ec6bd2c82d1f0aa935e02
Atul Tiwari <tiwariatul9526@gmail.com>
2024-11-12 23:12:16 +0530

    42. fetch login userDetails

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,19 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import fetchUserDetails from "./utils/fetchUserDetails";
 
 export default function App() {
+  const fetchUser = async () => {
+    const userData = await fetchUserDetails();
+    console.log("UserDate", userData.data);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Header />
