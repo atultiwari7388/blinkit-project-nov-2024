84fdba9e930e80e08a4a3a337dfaa8c970e85d1c
Atul Tiwari <tiwariatul9526@gmail.com>
2024-12-20 15:14:32 +0530

    87. getproduct by category and subcategory api done and route

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -2,6 +2,7 @@ import Router from "express";
 import {
   createProductController,
   getProductByCategory,
+  getProductByCategoryAndSubCategory,
   getProductController,
 } from "../controllers/product.controller.js";
 import auth from "../middleware/auth.js";
@@ -11,5 +12,9 @@ const productRouter = Router();
 productRouter.post("/create", auth, createProductController);
 productRouter.post("/get", getProductController);
 productRouter.post("/get-product-by-category", getProductByCategory);
+productRouter.post(
+  "/get-pruduct-by-category-and-subcategory",
+  getProductByCategoryAndSubCategory
+);
 
 export default productRouter;
