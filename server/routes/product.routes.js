156005fafd38cbadcd4efad211818c8e6da4d8b3
Atul Tiwari <tiwariatul9526@gmail.com>
2024-12-20 17:30:19 +0530

    93. admin middleware

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -12,7 +12,7 @@ import { admin } from "../middleware/admin.js";
 
 const productRouter = Router();
 
-productRouter.post("/create", auth, createProductController);
+productRouter.post("/create", auth, admin, createProductController);
 productRouter.post("/get", getProductController);
 productRouter.post("/get-product-by-category", getProductByCategory);
 productRouter.post(
